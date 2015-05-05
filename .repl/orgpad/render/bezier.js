1397349341c10236ecd417d6dba9b0490477a986
stredaa <adolf.streda@matfyz.cz>
2015-05-05 21:03:49 +0200

    file removal

--- .repl/orgpad/render/bezier.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-2814 {}
-goog.provide('orgpad.render.bezier');
-goog.require('cljs.core');
-orgpad.render.bezier.canvas = document.getElementById("myCanvas");
-orgpad.render.bezier.ctx = orgpad.render.bezier.canvas.getContext("2d");
--- target/js/out/boot/cljs/orgpad.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 0.0-2814 {}
-goog.provide('boot.cljs.orgpad');
-goog.require('cljs.core');
-goog.require('orgpad.render.metaball');
-goog.require('orgpad.render.bezier');
-goog.require('orgpad.core.boot');
-goog.require('adzerk.boot_reload');
-goog.require('adzerk.boot_cljs_repl');
-orgpad.core.boot.init.call(null);
