181ac428fc176c18511b90f49415ff72f8445064
stredaa <adolf.streda@matfyz.cz>
2015-05-12 07:48:40 +0200

    Render base, Bezier point fitting

--- src/orgpad/render/bezier.cljs
+++ src/orgpad/render/bezier.cljs
@@ -1,7 +1,7 @@
 (ns ^{:doc "Bezier rendering utilities"} orgpad.render.bezier )
 
+;drawing canvas ID
 (def canvas "mainCanvas")
-(def ctx (.getContext (.getElementById js/document canvas) "2d"))
 
 (defn norm [pt1 pt2] "Euclidian norm"
   (js/Math.sqrt
@@ -18,13 +18,20 @@
                 (pt2 1))
              2)))
 
-(defn draw-cubic-bezier [start cp0 cp1 end] "Draws bezier curve defined by 4 control points."
-  (do (.beginPath ctx)
-      (.moveTo ctx (start 0) (start 1))
-      (.bezierCurveTo ctx (cp0 0) (cp0 1) (cp1 0) (cp1 1) (end 0) (end 1))
-      (.stroke ctx)))
+(defn draw-cubic-bezier [pts] "Draws bezier curve defined by 4 control points."
+  (let [ctx (.getContext (.getElementById js/document canvas) "2d")]
+    (do (.beginPath ctx)
+        (.moveTo ctx ((nth pts 0) 0) ((nth pts 0) 1))
+        (.bezierCurveTo ctx 
+                        ((nth pts 1) 0) 
+                        ((nth pts 1) 1) 
+                        ((nth pts 2) 0) 
+                        ((nth pts 2) 1) 
+                        ((nth pts 3) 0) 
+                        ((nth pts 3) 1))
+        (.stroke ctx))))
 
-(defn get-cpts-3pts [sharpness start mid end] "Calculate two control points for cubic bezier from 3 points and sharpness factor, which affects the distance between control points."
+(defn get-cpts-3pts [sharpness start mid end direction] "Calculate two control points for cubic bezier from 3 points and sharpness factor, which affects the distance between control points."
   (let [shift (map - 
                    (midpoint start mid) 
                    (midpoint mid end))
@@ -32,21 +39,58 @@
                  (+ 1
                     (/ (norm start mid)
                        (norm mid end))))]
-    (vector (map +
-               mid
-               (for [x shift]
-                    (-> ratio (- 1) (* x sharpness))))
+    (into []
           (map -
                mid
                (for [x shift]
-                 (* x -1 ratio sharpness))))))
+               (* x direction ratio sharpness))))))
 
-(defn get-cpts-curve [sharpness pts] "Get vector of all cpts approximating closed curve. Change sharpness to fine-tune the approximation."
-  (if (>= (count pts) 3)
-      (let [cpts (get-cpts-3pts sharpness 
-                                (peek pts) 
-                                (-> pts pop peek) 
-                                (-> pts pop pop peek))]
-        (conj (get-cpts-curve sharpness (pop pts)) 
-              (-> pts pop peek) (cpts 0) (cpts 1)))
-      pts))
+(defn ^{:private true} cpts-closed-curve [sharpness pts] "Get vector of all cpts approximating closed curve. Change sharpness to fine-tune the approximation."
+  (if (>= (count pts) 4)
+      (conj (cpts-closed-curve sharpness (pop pts))
+            (list (-> pts pop peek)
+                  (get-cpts-3pts sharpness 
+                                 (peek pts) 
+                                 (-> pts pop peek) 
+                                 (-> pts pop pop peek)
+                                 1)
+                  (get-cpts-3pts sharpness 
+                                 (-> pts pop peek) 
+                                 (-> pts pop pop peek) 
+                                 (-> pts pop pop pop peek)
+                                 -1)
+                  (-> pts pop pop peek)))
+      (vector)))
+
+(defn ^{:private true} close-points [pts] "Prepends last two pts, appends first pts"
+  (conj (into (vector (-> pts pop peek) (peek pts))
+              pts)
+        (first pts) 
+        (-> pts rest first)))
+
+(defn draw-closed-curve 
+  ([sharpness pts]
+   (draw-closed-curve (cpts-closed-curve sharpness (close-points pts))))
+  ([pts]
+   (do (draw-cubic-bezier (peek pts))
+       (if (seq pts)
+         (draw-closed-curve (pop pts))))))
+
+;test Bezier
+(defn test-pts-gen [] 
+  (into [] 
+        (for [x (range 0 5)]
+          (vector (* (js/Math.random) 300) (* (js/Math.random) 150)))))
+(defn bezierTest []
+  (let [ctx (.getContext (.getElementById js/document canvas) "2d")
+        test-pts (test-pts-gen)]
+    (do
+      (.log js/console "Debug Bezier")
+      (.fillRect ctx ((test-pts 0) 0) ((test-pts 0) 1) 5 5)
+      (.fillRect ctx ((test-pts 1) 0) ((test-pts 1) 1) 5 5)
+      (.fillRect ctx ((test-pts 2) 0) ((test-pts 2) 1) 5 5)
+      (.fillRect ctx ((test-pts 3) 0) ((test-pts 3) 1) 5 5)
+      (.fillRect ctx ((test-pts 4) 0) ((test-pts 4) 1) 5 5)
+;      (for [x (range 1 (count test-pts))]
+;        (.fillRect ctx ((test-pts x) 0) ((test-pts x) 1) 5 5))
+      (draw-closed-curve 1 test-pts))))
--- src/orgpad/render/metaball.cljs
+++ src/orgpad/render/metaball.cljs
@@ -1,30 +1,30 @@
 (ns ^{:doc "Blob calculation utilities"} orgpad.render.metaball ) ;blob utilities
 
 (defn norm-squared [pt1 pt2]
-  (+ (* (- (pt1 0) (pt2 0)) 
-        (- (pt1 0) (pt2 0)))
-     (* (- (pt1 1) (pt2 1)) 
-        (- (pt1 1) (pt2 1)))))
+    (+ (* (- (pt1 0) (pt2 0)) 
+                  (- (pt1 0) (pt2 0)))
+            (* (- (pt1 1) (pt2 1)) 
+                       (- (pt1 1) (pt2 1)))))
 
 ;tested
 (defn blob-fcion [centre, pt, size] "Single blob function."
-  (/ size (norm-squared pt centre)))
+    (/ size (norm-squared pt centre)))
 
 ;tested
 (defn total-blob-fcion [pt blobs]
-  (let [temp (peek blobs)]
-    (if (peek blobs)
-        (conj (total-blob-fcion pt (pop blobs))
-              (blob-fcion (get temp :centre) pt (get temp :size)))
-        (vector))))
+    (let [temp (peek blobs)]
+          (if (peek blobs)
+                    (conj (total-blob-fcion pt (pop blobs))
+                                        (blob-fcion (get temp :centre) pt (get temp :size)))
+                            (vector))))
 
 ;tested
 (defn resize-blobs [size blobs]
-  (let [temp (peek blobs)]
-    (if (peek blobs)
-        (conj (resize-blobs size (pop blobs))
-              (assoc temp :size (* size (get temp :size))))
-        (vector))))
+    (let [temp (peek blobs)]
+          (if (peek blobs)
+                    (conj (resize-blobs size (pop blobs))
+                                        (assoc temp :size (* size (get temp :size))))
+                            (vector))))
 
 ;Cljs does not support macros - TODO move to external clj file
 ;(defmacro transform [expression objects] "A macro applying expression to every element of a vector. Use \"x\" as a target of the expression"
@@ -36,42 +36,41 @@
 
 ;tested - TODO further fine-tuning required, postponed till commiting rendering functions
 (defn shift [centre1 centre2] "Shift vector of the length cca 20, perpendicular to the line between two points."
-  (let [norm (js/Math.sqrt (norm-squared centre1 centre2))]
-    (vector (- (/ (* 20 
-                     (- 
-                       (centre1 0) 
-                       (centre2 0)))
-                  norm))
-            (/ (* 20 
-                  (- (centre1 1)
-                     (centre2 1)))
-               norm))
-  )
-)
+    (let [norm (js/Math.sqrt (norm-squared centre1 centre2))]
+          (vector (- (/ (* 20 
+                                                (- 
+                                                                          (centre1 0) 
+                                                                                                 (centre2 0)))
+                                          norm))
+                              (/ (* 20 
+                                                      (- (centre1 1)
+                                                                              (centre2 1)))
+                                                norm))
+            )
+    )
 
 ;tested
 (defn get-coef [i j blobs] "Calculates the minimal size coef. required by blob conectness."
-  (let [correction (shift (get (blobs i) :centre) 
-                          (get (blobs j) :centre))
-        pt (vector (+ (/ (+ (-> (blobs i) :centre first)
-                            (-> (blobs j) :centre first))
-                       2)
-                    (correction 0))
-                 (+ (/ (+ (-> (blobs i) :centre last)
-                          (-> (blobs j) :centre last))
-                       2)
-                    (correction 1)))]
-    (/ 1
-       (reduce + (total-blob-fcion pt blobs)))))
+    (let [correction (shift (get (blobs i) :centre) 
+                                                      (get (blobs j) :centre))
+                  pt (vector (+ (/ (+ (-> (blobs i) :centre first)
+                                                                  (-> (blobs j) :centre first))
+                                                          2)
+                                                    (correction 0))
+                                              (+ (/ (+ (-> (blobs i) :centre last)
+                                                                                 (-> (blobs j) :centre last))
+                                                                           2)
+                                                                     (correction 1)))]
+          (/ 1
+                    (reduce + (total-blob-fcion pt blobs)))))
 
 ;tested
 (defn calc-blob [& blob-list] "Adjusts the size of given blobs to create a connected metaball."
-  (let [blobs (into [] blob-list)
-        coef (reduce max (for [i (range 0 (count blobs))
-                               j (range 0 (count blobs))
-                              :when (> i j)]
-                              (get-coef i j blobs)))]
-  (do (print coef blobs) 
-    (resize-blobs (max 1 coef) blobs)))
-  )
-
+    (let [blobs (into [] blob-list)
+                  coef (reduce max (for [i (range 0 (count blobs))
+                                                                        j (range 0 (count blobs))
+                                                                                                      :when (> i j)]
+                                                                   (get-coef i j blobs)))]
+        (do (print coef blobs) 
+                (resize-blobs (max 1 coef) blobs)))
+      )
