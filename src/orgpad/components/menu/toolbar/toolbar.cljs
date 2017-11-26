c8d5bb039e08b2cd67309b3bfaa624368ed00b3c
Pavel Klavik <klavik@kam.mff.cuni.cz>
2017-11-26 23:18:26 +0100

    Changed icon style and new icon choosen.

--- src/orgpad/components/menu/toolbar/toolbar.cljs
+++ src/orgpad/components/menu/toolbar/toolbar.cljs
@@ -53,7 +53,7 @@
         {:className button-class
          :title title
          :onMouseDown on-mouse-down }
-         [:i { :className (str "fa " icon " fa-lg fa-fw") }]
+         [:i { :className (str "far " icon " fa-lg fa-fw") }]
          (when label [:span.btn-icon-label label])]
       [:span
         {:className button-class
@@ -76,7 +76,7 @@
     [:span.lft-roll-btn
      {:title "Roll test"
       :onMouseDown (jev/make-block-propagation #(swap! open toggle-open-state :test))}
-      [:i { :className "fa fa-plus-circle fa-lg fa-fw" }]
+      [:i { :className "far fa-plus-circle fa-lg fa-fw" }]
       [:span.btn-icon-text "Roll button"]
       [:i { :className "fa fa-caret-down" }]]
     (when (= @open :test)
@@ -84,12 +84,12 @@
         [:span.roll-item
          {:title "Roll item 1"
           :onMouseDown #(close-roll open (fn [] (js/console.log "Roll item 1 pressed")))}
-          [:i.fa.fa-columns.fa-lg.fa-fw]
+          [:i.far.fa-columns.fa-lg.fa-fw]
           [:span.roll-icon-label "Notebook view"]]
         [:span.roll-item
          {:title "Roll item 2"
           :onMouseDown #(js/console.log "Roll item 2 pressed")}
-          [:i.fa.fa-window-restore.fa-lg.fa-fw]
+          [:i.far.fa-share-alt.fa-lg.fa-fw]
           [:span.roll-icon-label "Map view"]]
         [:span.roll-item
          {:title "Roll item 3"
@@ -112,20 +112,20 @@
      [:span.lft-btn
       {:title "Previous page"
        :onMouseDown #(omt/switch-active-sheet component unit-tree -1) }
-      [:i.fa.fa-arrow-left.fa-lg.fa-fw]]
+      [:i.far.fa-arrow-left.fa-lg.fa-fw]]
      [:span.lft-btn
       {:title "Next page"
        :onMouseDown #(omt/switch-active-sheet component unit-tree 1) }
-      [:i.fa.fa-arrow-right.fa-lg.fa-fw]]
+      [:i.far.fa-arrow-right.fa-lg.fa-fw]]
      [:span.lft-text (apply gstring/format "%d/%d" (ot/get-sheet-number unit-tree))]
      [:span.lft-btn
       {:title "Add page"
        :onMouseDown #(omt/new-sheet component unit-tree) }
-      [:i.fa.fa-plus-circle.fa-lg.fa-fw]]
+      [:i.far.fa-plus-circle.fa-lg.fa-fw]]
      [:span.lft-btn
       {:title "Remove page"
        :onMouseDown #(omt/remove-active-sheet component unit-tree) }
-      [:i.fa.fa-minus-circle.fa-lg.fa-fw]]
+      [:i.far.fa-minus-circle.fa-lg.fa-fw]]
      (let [ ac-unit-tree (ot/active-child-tree unit view)
             ac-view-type (ot/view-type ac-unit-tree)
             class-sheet (str "lft-btn" (when (= ac-view-type :orgpad/atomic-view) " active"))
@@ -135,12 +135,12 @@
           {:className class-sheet
            :title "Sheet"
            :onMouseDown #(omt/change-view-type component ac-unit-tree :orgpad/atomic-view) }
-           [:i.fa.fa-file-text-o.fa-lg.fa-fw]]
+           [:i.far.fa-file-alt.fa-lg.fa-fw]]
          [:span
           {:className class-map
            :title "Map"
            :onMouseDown #(omt/change-view-type component ac-unit-tree :orgpad/map-view) }
-          [:i.fa.fa-window-restore.fa-lg.fa-fw]]))])
+          [:i.far.fa-share-alt.fa-lg.fa-fw]]))])
 
 (defn- add-view-buttons
   [component unit-tree]
@@ -152,12 +152,12 @@
       { :className class-notebook
        :title "Notebook"
        :onMouseDown #(omt/change-view-type component unit-tree :orgpad/map-tuple-view) }
-      [:i.fa.fa-columns.fa-lg.fa-fw]]
+      [:i.far.fa-book.fa-lg.fa-fw]]
      [:span
       { :className class-map
        :title "Map"
        :onMouseDown #(omt/change-view-type component unit-tree :orgpad/map-view) }
-      [:i.fa.fa-window-restore.fa-lg.fa-fw]]
+      [:i.far.fa-share-alt.fa-lg.fa-fw]]
      (when (= view-type :orgpad/map-tuple-view)
       (add-notebook-manipulators component unit-tree))
      [:span.lft-sep]]))
@@ -169,19 +169,19 @@
       { :title "Link"
         :onMouseDown (jev/make-block-propagation #(omt/start-link local-state %))
         :onTouchStart (jev/make-block-propagation #(omt/start-link local-state (aget % "touches" 0)))}
-     [:i.fa.fa-link.fa-lg.fa-fw]]
+     [:i.far.fa-link.fa-lg.fa-fw]]
     [:span.lft-btn
       { :title "Edit"
         :onMouseDown jev/block-propagation
         :onMouseUp (jev/make-block-propagation #(omt/open-unit component unit-tree))}
-     [:i.fa.fa-pencil-square-o.fa-lg.fa-fw]]
+     [:i.far.fa-edit.fa-lg.fa-fw]]
     [:span.lft-sep]
     (add-view-buttons component unit-tree)
 
     [:span.rt-btn
       { :title "Remove"
         :onMouseDown #(omt/remove-unit component (ot/uid unit-tree))}
-     [:i.fa.fa-remove.fa-lg.fa-fw]]])
+     [:i.far.fa-trash-alt.fa-lg.fa-fw]]])
 
 (defn- render-map-tools
   [local-state-atom]
@@ -194,17 +194,17 @@
        {:className class-create
         :title "Unit creation mode"
         :onClick #(swap! local-state-atom assoc :canvas-mode :canvas-create-unit)}
-        [:i.fa.fa-plus.fa-lg.fa-fw]]
+        [:i.far.fa-plus-square.fa-lg.fa-fw]]
       [:span
        {:className class-move
         :title "Moving mode"
         :onClick #(swap! local-state-atom assoc :canvas-mode :canvas-move)}
-        [:i.fa.fa-arrows.fa-lg.fa-fw]]
+        [:i.far.fa-arrows.fa-lg.fa-fw]]
       [:span
        {:className class-select
         :title "Selection mode"
         :onClick #(swap! local-state-atom assoc :canvas-mode :canvas-select)}
-        [:i.fa.fa-crop.fa-lg.fa-fw]]
+        [:i.far.fa-expand.fa-lg.fa-fw]]
       [:span.lft-sep]]))
 
 (defn- render-copy-tools
@@ -214,12 +214,12 @@
       [:span.lft-btn
        {:title "Copy"
         :onClick #(omt/copy-units-to-clipboard component unit-tree app-state)}
-        [:i.fa.fa-copy.fa-lg.fa-fw]]
+        [:i.far.fa-copy.fa-lg.fa-fw]]
       [:span
        {:className class-paste
         :title "Paste"
         :onMouseDown #(swap! local-state-atom assoc :local-mode :canvas-paste)}
-        [:i.fa.fa-paste.fa-lg.fa-fw]]
+        [:i.far.fa-paste.fa-lg.fa-fw]]
       [:span.lft-sep]]))
 
 (rum/defcs app-toolbar < (rum/local nil ::open)
