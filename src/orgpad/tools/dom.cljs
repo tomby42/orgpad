32850b06bfb36893d7d023786adf86224514f2be
Pavel Klavik <klavik@kam.mff.cuni.cz>
2018-10-22 11:30:49 +0200

    Fixing resize for one-line units.

--- src/orgpad/tools/dom.cljs
+++ src/orgpad/tools/dom.cljs
@@ -95,7 +95,7 @@
 
 (defn- gen-wrapped-html
   [{:keys [html width id]}]
-  (str "<div style=\"width: " width "px; display: flex;\"><div id=\"" id "\" style=\"overflow-y: scroll; overflow-x: hidden;\">" html "</div></div>\n"))
+  (str "<div style=\"width: " width "px; display: flex;\"><div id=\"" id "\" style=\"overflow-y: scroll; overflow-x: visible;\">" html "</div></div>\n"))
 
 (defn- gen-wrapped-htmls
   [{:keys [html widths id]}]
